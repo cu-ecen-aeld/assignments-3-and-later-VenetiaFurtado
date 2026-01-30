81b42d2a5c03506e9f7bd6c93a6ca25032ecabc7
venetia <venetiarosita@gmail.com>
2026-01-30 03:32:59 +0000

    do_exec_redirect() pass

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "systemcalls.h"
 
 /**
@@ -11,16 +13,16 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
+ */
 bool do_system(const char *cmd)
 {
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-    if(cmd == NULL)
+    /*
+     * TODO  add your code here
+     *  Call the system() function with the command set in the cmd
+     *   and return a boolean true if the system() call completed with success
+     *   or false() if it returned a failure
+     */
+    if (cmd == NULL)
     {
         return false;
     }
@@ -28,24 +30,24 @@ bool do_system(const char *cmd)
     /*This code has been referenced from Linux System Programming (Chapter 5 - Waiting for Terminated Child processes)*/
     int status = system(cmd);
 
-    //check if invocation of system() call fails
-    if(status == -1)
+    // check if invocation of system() call fails
+    if (status == -1)
     {
         perror("system");
         return false;
     }
 
-    //check if system() call completed successfully
-    //WIFEXITED(status): returns true if the child terminated normally
-    //WEXITSTATUS(status): returns the exit status of the child. This macro is
-    // employed only if WIFEXITED returned true.
-    //Source: https://www.geeksforgeeks.org/linux-unix/exit-status-child-process-linux/
+    // check if system() call completed successfully
+    // WIFEXITED(status): returns true if the child terminated normally
+    // WEXITSTATUS(status): returns the exit status of the child. This macro is
+    //  employed only if WIFEXITED returned true.
+    // Source: https://www.geeksforgeeks.org/linux-unix/exit-status-child-process-linux/
     if (WIFEXITED(status))
     {
         int exit_status = WEXITSTATUS(status);
         printf("Process exited normally with exit code %d\n", exit_status);
 
-        if(exit_status == 0)
+        if (exit_status == 0)
         {
             return true;
         }
@@ -56,26 +58,26 @@ bool do_system(const char *cmd)
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
@@ -84,26 +86,28 @@ bool do_exec(int count, ...)
     // and may be removed
     command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    /*
+     * TODO:
+     *   Execute a system command by calling fork, execv(),
+     *   and wait instead of system (see LSP page 161).
+     *   Use the command[0] as the full path to the command to execute
+     *   (first argument to execv), and use the remaining arguments
+     *   as second argument to the execv() command.
+     *
+     */
+    // The code below has been referenced from LSP Chapter 5:
+    // Code snippets under "fork() system call" and  "Launching and Waiting for a new process"
     pid_t child_pid = fork();
 
-    if (child_pid == -1) 
+    if (child_pid == -1)
     {
         // fork failed
         va_end(args);
         return false;
     }
 
-    //child process
-    if (child_pid == 0) 
+    // child process
+    if (child_pid == 0)
     {
         execv(command[0], command);
 
@@ -114,7 +118,7 @@ bool do_exec(int count, ...)
     // Parent process
     int status;
 
-    if (waitpid(child_pid, &status, 0) == -1) 
+    if (waitpid(child_pid, &status, 0) == -1)
     {
         // waitpid failed
         va_end(args);
@@ -122,7 +126,7 @@ bool do_exec(int count, ...)
     }
 
     // Check if child exited normally and with status 0
-    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) 
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
     {
         va_end(args);
         return true;
@@ -133,17 +137,17 @@ bool do_exec(int count, ...)
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
@@ -152,16 +156,69 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
+    /*
+     * TODO
+     *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+     *   redirect standard out to a file specified by outputfile.
+     *   The rest of the behaviour is same as do_exec()
+     *
+     */
+    // The code below has been referenced from LSP Chapter 5:
+    // Code snippets under "fork() system call" and  "Launching and Waiting for a new process"
+    pid_t child_pid = fork();
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    if (child_pid == -1)
+    {
+        // fork failed
+        va_end(args);
+        return false;
+    }
+
+    // child process
+    if (child_pid == 0)
+    {
+        // Open the output file
+        //Source: https://stackoverflow.com/questions/13784269/redirection-inside-call-to-execvp-not-working/13784315#13784315
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) 
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        // Redirect stdout to the file
+        if (dup2(fd, STDOUT_FILENO) < 0) 
+        {
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Close duplicated fd
+        close(fd);
+
+        execv(command[0], command);
+
+        // If execv returns, it failed
+        exit(EXIT_FAILURE);
+    }
+
+    // Parent process
+    int status;
+
+    if (waitpid(child_pid, &status, 0) == -1)
+    {
+        // waitpid failed
+        va_end(args);
+        return false;
+    }
+
+    // Check if child exited normally and with status 0
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+    {
+        va_end(args);
+        return true;
+    }
 
     va_end(args);
 
-    return true;
+    return false;
 }
