5ebc382715aec67cc650eaf3ede756d1cd97fa9a
venetia <venetiarosita@gmail.com>
2026-02-01 18:10:45 +0000

    cleanup

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,9 +13,6 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-# Saves the location of the script
-SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
-
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -37,7 +34,8 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Add your kernel build steps here
+    # Reference: ECEN 5713 Coursera slides
     make defconfig
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
@@ -57,7 +55,8 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+# Reference: ECEN 5713 Coursera slides
 mkdir rootfs
 cd rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
@@ -71,14 +70,16 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    # Reference: ECEN 5713 Coursera slides
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+# Reference: ECEN 5713 Coursera slides
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
@@ -87,35 +88,37 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a rootfs/bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a rootfs/bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+# Reference: https://chatgpt.com/share/697f7555-60c0-8001-80b8-e129a620cbc4
 cp $(${CROSS_COMPILE}gcc -print-sysroot)/lib/ld-linux-aarch64.so.1 rootfs/lib/
 cp $(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libm.so.6 rootfs/lib64/
 cp $(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libresolv.so.2 rootfs/lib64/
 cp $(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libc.so.6  rootfs/lib64/
 
-# TODO: Make device nodes
+# Make device nodes
 sudo mknod -m 666 rootfs/dev/null c 1 3
 sudo mknod -m 600 rootfs/dev/console c 5 1
 
-# TODO: Clean and build the writer utility
-cd "$SCRIPT_DIR"
+# Clean and build the writer utility
+cd "$FINDER_APP_DIR"
 make clean
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 cd "$OUTDIR"
-cp ${SCRIPT_DIR}/writer rootfs/home/
-cp ${SCRIPT_DIR}/finder.sh rootfs/home/
-cp ${SCRIPT_DIR}/finder-test.sh rootfs/home/
+cp ${FINDER_APP_DIR}/writer rootfs/home/
+cp ${FINDER_APP_DIR}/finder.sh rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh rootfs/home/
+# Reference: https://chatgpt.com/share/697f7555-60c0-8001-80b8-e129a620cbc4
 sed -i 's|`cat \.\./conf/assignment.txt`|`cat conf/assignment.txt`|' rootfs/home/finder-test.sh
-cp ${SCRIPT_DIR}/autorun-qemu.sh rootfs/home/
-cp -r ${SCRIPT_DIR}/../conf rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh rootfs/home/
+cp -r ${FINDER_APP_DIR}/../conf rootfs/home/
 
-# TODO: Chown the root directory
+# Chown the root directory
 sudo chown -R root:root rootfs
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
 cd "$OUTDIR/rootfs"
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 
