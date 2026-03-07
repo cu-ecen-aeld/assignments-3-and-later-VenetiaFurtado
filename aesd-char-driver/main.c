3d485c82857397dad1121ab1734ea0fe83e312f1
Venetia Furtado <venetiarosita@gmail.com>
2026-03-07 17:38:29 +0000

    aesd_write

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -32,7 +32,7 @@ int aesd_open(struct inode *inode, struct file *filp)
     /**
      * TODO: handle open
      */
-    struct aesd_dev* dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+    struct aesd_dev *dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
     filp->private_data = dev;
     return 0;
 }
@@ -60,11 +60,84 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                    loff_t *f_pos)
 {
-    ssize_t retval = -ENOMEM;
+    if (filp == NULL || buf == NULL || fpos == NULL)
+    {
+        PDEBUG("ERROR: input argument error");
+        return -EINVAL;
+    }
+
+    ssize_t retval = 0;
     PDEBUG("write %zu bytes with offset %lld", count, *f_pos);
-    /**
-     * TODO: handle write
-     */
+
+    // allocate memory
+    char *kbuf = kmalloc(count, GFP_KERNEL);
+    if (kbuf == NULL)
+    {
+        PDEBUG("ERROR: Cannot allocate memory for new data");
+        return -ENOMEM;
+    }
+
+    // copy from user to kernel space
+    unsigned long status = copy_from_user(kbuf, buf, count);
+    if (status != 0)
+    {
+        PDEBUG("ERROR: copy from userspace failed");
+        return -EFAULT;
+    }
+
+    // check if data contains newline
+    bool write_to_cb = false;
+    for (size_t i = 0; i < count; i++)
+    {
+        if (kbuf[i] == '\n')
+        {
+            write_to_cb = true;
+            break;
+        }
+    }
+
+    struct aesd_dev *dev = filp->private_data;
+    if (dev == NULL)
+    {
+        PDEBUG("ERROR: device driver pointer is NULL");
+        return -EINVAL;
+    }
+
+    // start critical section
+    mutex_lock(&dev->lock);
+
+    // save data for future write operations if no newline found
+    dev->new_entry.buffptr = krealloc(dev->new_entry.buffptr, dev->new_entry.size + count, GFP_KERNEL);
+    if (dev->new_entry.buffptr == NULL)
+    {
+        kfree(kbuf);
+        PDEBUG("ERROR: krealloc failed");
+        mutex_unlock(&dev->lock);
+        return -ENOMEM;
+    }
+    memcpy((char *)dev->new_entry.buffptr + dev->new_entry.size, kbuf, count);
+    dev->new_entry.size += count;
+
+    // write to cb since newline was found
+    if (write_to_cb == true)
+    {
+        const char *buffptr_to_free = aesd_circular_buffer_add_entry(&(dev->buffer), &(dev->new_entry));
+
+        if (buffptr_to_free != NULL)
+        {
+            kfree(buffptr_to_free);
+            PDEBUG("WARN: kfree of old buffer failed");
+        }
+
+        dev->new_entry.buffptr = NULL;
+        dev->new_entry.size = 0;
+    }
+    mutex_unlock(&dev->lock);
+    // end critical section
+
+    // set retval to count to signal that requested number of bytes has been transferred
+    retval = count;
+
     return retval;
 }
 struct file_operations aesd_fops = {
@@ -108,6 +181,8 @@ int aesd_init_module(void)
      * TODO: initialize the AESD specific portion of the device
      */
     aesd_circular_buffer_init(&(aesd_device.buffer));
+    aesd_device.new_entry.buffptr = NULL;
+    aesd_device.new_entry.size = 0;
     mutex_init(&aesd_device.lock);
 
     result = aesd_setup_cdev(&aesd_device);
